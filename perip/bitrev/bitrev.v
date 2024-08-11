8a4b125fd56b906d961732c527fb476ec5cff266
202280542-Wen jiu <2784307979@qq.com>
2024-08-11 16:08:51 +0900

    完成了bit-rev模块的实现和flash的xip功能

--- perip/bitrev/bitrev.v
+++ perip/bitrev/bitrev.v
@@ -4,5 +4,47 @@ module bitrev (
   input  mosi,
   output miso
 );
-  assign miso = 1'b1;
+  wire reset = ss;
+
+  typedef enum [1:0] { s_receive, s_transmit} state_t;
+  reg[1:0] state;
+  reg[2:0] counter;
+
+  always@(posedge sck or posedge reset) begin
+    if (reset) begin 
+      state <= s_receive; 
+    end
+    else begin
+        case (state)
+          s_receive:  state <= (counter == 3'd7) ? s_transmit : state;
+          s_transmit: state <= (counter == 3'd0) ? s_receive  : state;
+          default:    state <= s_receive;
+        endcase
+    end 
+  end
+
+  always@(posedge sck or posedge reset) begin
+    if (reset) counter <= 3'd0;
+    else begin
+      case (state)
+        s_receive:  counter <= (counter < 3'd7) ? counter + 3'd1 : counter;
+        s_transmit: counter <= (counter > 3'd0) ? counter - 3'd1 : counter;
+        default:    counter <= counter + 3'd1;
+      endcase
+    end
+  end 
+
+  reg [7:0] stack;
+
+  always@(posedge sck or posedge reset) begin
+    if (reset) stack <= 8'd0;
+    else begin
+      case (state)
+        s_receive:  stack[counter] <= mosi;
+        default:    stack[counter] <= stack[counter];
+      endcase
+    end
+  end
+
+  assign miso = ((state == s_receive) || reset) ? 1'b1 : stack[counter];
 endmodule
