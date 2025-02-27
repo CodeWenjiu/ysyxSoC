9110f2529520cce1a34e7c009057333ec137b09a
wenjiu <2784307979@qq.com>
2025-02-27 23:02:39 +0800

    feat: simple AXI delayer(need to be fix

--- src/amba/AXI4Delayer.scala
+++ src/amba/AXI4Delayer.scala
@@ -23,6 +23,66 @@ class axi4_delayer extends BlackBox {
 class AXI4DelayerChisel extends Module {
   val io = IO(new AXI4DelayerIO)
   io.in <> io.out
+
+  def r = 985 //Mhz
+  val s_count :: s_wait :: Nil = Enum(2)
+
+  val state_r = RegInit(s_wait)
+  val delay_cnt_read = RegInit(0.U(16.W))
+  val exact_delay_cnt_read = RegInit(0.U(7.W))
+  val delay_target_read = RegInit(0.U(16.W))
+
+  val state_w = RegInit(s_wait)
+  val delay_cnt_write = RegInit(0.U(16.W))
+  val exact_delay_cnt_write = RegInit(0.U(7.W))
+  val delay_target_write = RegInit(0.U(16.W))
+
+  state_r := MuxLookup(state_r, s_wait)(Seq(
+    s_count -> Mux(io.out.r.valid, s_wait, s_count),
+    s_wait -> Mux(delay_cnt_read === delay_target_read, s_count, s_wait)
+  ))
+
+  state_w := MuxLookup(state_w, s_wait)(Seq(
+    s_count -> Mux(io.out.b.valid, s_wait, s_count),
+    s_wait -> Mux(delay_cnt_write === delay_target_write, s_count, s_wait)
+  ))
+
+  when(io.in.ar.fire) {
+    delay_cnt_read := 0.U
+    delay_target_read := 0.U
+  }
+
+  when(state_r === s_count) {
+    exact_delay_cnt_read := Mux(exact_delay_cnt_read === 99.U, 0.U, exact_delay_cnt_read + 1.U)
+    delay_cnt_read := delay_cnt_read + 1.U
+    delay_target_read := delay_target_read + Mux(exact_delay_cnt_read === (r % 100 - 1).U, (r / 100).U, ((r / 100) + 1).U)
+  }.elsewhen(state_r === s_wait){
+    delay_cnt_read := delay_cnt_read + 1.U
+  }
+
+  when(RegNext(state_w) === s_wait && state_w === s_count) {
+    delay_cnt_write := 0.U
+    delay_target_write := 0.U
+  }
+
+  when(state_w === s_count) {
+    exact_delay_cnt_write := Mux(exact_delay_cnt_write === 99.U, 0.U, exact_delay_cnt_write + 1.U)
+    delay_cnt_write := delay_cnt_write + 1.U
+    delay_target_write := delay_target_write + Mux(exact_delay_cnt_write === (r % 100 - 1).U, (r / 100).U, ((r / 100) + 1).U)
+  }.elsewhen(state_w === s_wait){
+    delay_cnt_write := delay_cnt_write + 1.U
+  }
+
+  when(state_r === s_wait){
+    io.in.r.valid := false.B
+    io.out.r.ready := false.B
+  }
+
+  when(state_w === s_wait){
+    io.in.b.valid := false.B
+    io.out.b.ready := false.B
+  }
+
   // def r = 10
 
   // val s_idle :: s_wait_resp :: s_delay :: s_transmit :: Nil = Enum(4)
